24723dc20174e652a11b06413a23b72deb2ff673
annomao <omaoannkerubo@gmail.com>
2022-10-03 09:07:05 +0300

    add web security configurations

--- src/main/java/ke/co/safaricom/Security_demo/config/WebSecurityConfig.java
+++ src/main/java/ke/co/safaricom/Security_demo/config/WebSecurityConfig.java
@@ -1,4 +1,49 @@
 package ke.co.safaricom.Security_demo.config;
 
-public class WebSecurityConfig {
+import ke.co.safaricom.Security_demo.services.MyUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new MyUserDetailsService();
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+   public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws  Exception{
+        http.authorizeHttpRequests().antMatchers("/api/v1/users").permitAll()
+                .antMatchers("/api/v1/users/{id}", "/api/v1/bookings").hasAnyRole("USER","ADMIN")
+                .antMatchers("/api/v1/bookings/{id}").hasRole("USER")
+                .and().formLogin()
+                .and().logout();
+        http.csrf().disable();
+    }
+
 }
